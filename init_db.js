7a20342ec8102d4290408ce1d57bb25273a33c1f
madezainul <madezainul@gmail.com>
2025-03-27 07:29:13 +0300

    fixed DB

--- init_db.js
+++ init_db.js
@@ -7,33 +7,22 @@ const createUsersTable = `CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )`;
 
-const createReportHeaderTable = `CREATE TABLE IF NOT EXISTS report_headers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    report_id INT NOT NULL,
-    report_date DATE NOT NULL,
-    report_description VARCHAR(50) NOT NULL,
-    supervisor_name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)`;
-
 const createReportDetailTable = `CREATE TABLE IF NOT EXISTS report_details (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    report_header_id INT NOT NULL,
-    date DATE NOT NULL,
-    equipment_name VARCHAR(100) NOT NULL,
-    equipment_id VARCHAR(100) NOT NULL,
-    equipment_model VARCHAR(100) NOT NULL,
-    problem_description TEXT NOT NULL,
-    solution_part_replaced TEXT NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    start_time TIME NOT NULL,
-    stop_time TIME NOT NULL,
-    total_time_spent INT NOT NULL,
-    technician_name VARCHAR(100) NOT NULL,
-    supervisor_name VARCHAR(100) NOT NULL,
-    category VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (report_header_id) REFERENCES report_headers(id)
+    date DATE,
+    shift VARCHAR(2),
+    equipment_name VARCHAR(100),
+    equipment_id VARCHAR(100),
+    problem_description TEXT,
+    solution_part_replaced TEXT,
+    status VARCHAR(10),
+    start_time TIME,
+    stop_time TIME,
+    total_time_spent INT,
+    technician_name VARCHAR(100),
+    supervisor_name VARCHAR(100),
+    category VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )`;
 
 connection.query(createUsersTable, (err, results, fields) => {
@@ -42,14 +31,6 @@ connection.query(createUsersTable, (err, results, fields) => {
         return;
     }
     console.log('Created users table successfully');
-});
-
-connection.query(createReportHeaderTable, (err, results, fields) => {
-    if (err) {
-        console.error('Error creating report_headers table', err);
-        return;
-    }
-    console.log('Created report_headers table successfully');
 });
 
 connection.query(createReportDetailTable, (err, results, fields) => {
