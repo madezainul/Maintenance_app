32ad3a10babb5ea64f6283e26bc5bcb7b9424013
madezainul <madezainul@gmail.com>
2025-04-03 09:28:12 +0300

    Fix the routes home and fix report page and report details

--- app/routes/Home.js
+++ app/routes/Home.js
@@ -7,26 +7,33 @@ const express = require('express'),
 	{ ReportDetail } = require('../models/ReportDetailModel'),
 	{ Auth } = require('../middlewares/Auth');
 
+// router.get('/', async (req, res) => {
+// 	ReportDetail.all((err, rows) => {
+// 		let context = {
+// 			title: 'Report Details',
+// 			reports: rows.map(row => {
+// 				return {
+// 					...row, 
+// 					date: moment(row.date).format('YYYY MMMM'),
+// 					year: moment(row.date).format('YYYY'),
+// 					month: moment(row.date).format('MMMM')
+// 				}
+// 			})
+// 		};
+// 		res.render('home/index', context);
+// 	});
+// });
 router.get('/', async (req, res) => {
-	ReportDetail.all((err, rows) => {
 		let context = {
-			title: 'Report Details',
-			reports: rows.map(row => {
-				return {
-					...row, 
-					date: moment(row.date).format('YYYY MMMM'),
-					year: moment(row.date).format('YYYY'),
-					month: moment(row.date).format('MMMM')
-				}
-			})
+			title: 'Home',
 		};
 		res.render('home/index', context);
 	});
-});
+
 
 router.get('/help', Auth.isUser, async (req, res) => {
 	let context = {
-		title: 'Bantuan',
+		title: 'Help',
 	};
 	res.render('home/help', context);
 });
